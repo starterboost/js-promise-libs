edb675aef444ac15e5dfd9ba3333a1c580f98565
jimib <jimi.bailey@gmail.com>
2019-07-05 22:58:57 +0100

    throwIfFulfilled added as option on PromiseResolver

--- libs/promise-resolver.js
+++ libs/promise-resolver.js
@@ -1,6 +1,8 @@
 const Promise = require('bluebird');
 
-function PromiseResolver(){
+function PromiseResolver( options = {} ){
+	this.throwIfFulfilled = options.throwIfFulfilled == false ? false : true;
+
 	this.promise = new Promise( ( resolve, reject ) => {
 		this._resolve = resolve;
 		this._reject = reject;
@@ -16,13 +18,17 @@ PromiseResolver.prototype.isPending = function(){
 }
 
 PromiseResolver.prototype.resolve = function( value ){
-	if( this.isFulfilled() ) throw new Error('Already fulfilled promise');
-	return this._resolve( value );
+	//only throw if told to throw
+	if( this.throwIfFulfilled && this.isFulfilled() ) throw new Error('Already fulfilled promise');
+	//if pending then resolve the promose - otherwise create new
+	return this.isPending() ? this._resolve( value ) : Promise.resolve( value );
 }
 
 PromiseResolver.prototype.reject = function( err ){
-	if( this.isFulfilled() ) throw new Error('Already fulfilled promise');
-	return this._reject( err );
+	//only throw if told to throw
+	if( this.throwIfFulfilled && this.isFulfilled() ) throw new Error('Already fulfilled promise');
+	//if pending then reject the promose - otherwise create new
+	return this.isPending() ? this._reject( err ) : Promise.reject( err );
 }
 
 module.exports = PromiseResolver;
